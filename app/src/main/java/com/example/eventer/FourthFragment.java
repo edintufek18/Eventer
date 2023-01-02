c431385ce51a3acb20498ad97afedc3d066cf79f
Nastasija <Nastasija@gmail.com>
2023-01-02 17:21:14 +0100

    get request for product

--- app/src/main/java/com/example/eventer/FourthFragment.java
+++ app/src/main/java/com/example/eventer/FourthFragment.java
@@ -1,5 +1,6 @@
 package com.example.eventer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.eventer.databinding.FragmentFourthBinding;
 
 import org.json.JSONArray;
@@ -19,14 +25,18 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 
 public class FourthFragment extends Fragment{
     private FragmentFourthBinding binding;
+    private RequestQueue requestQueue;
+    private String url = "https://tilenkelc.eu/Eventer/api/categories";
+    private String categID;
 
     private String token = "";
-    ;
+
 
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -36,13 +46,71 @@ public class FourthFragment extends Fragment{
         binding = FragmentFourthBinding.inflate(inflater, container, false);
 
         token = ((GlobalClass) getActivity().getApplication()).getToken();
-
         System.out.println("new" + token);
+        requestQueue = Volley.newRequestQueue(requireActivity().getApplicationContext());
+        categID = ((GlobalClass) getActivity().getApplication()).getCategory();
+        prikaziKategorije();
+
+
 
         return binding.getRoot();
 
 
     }
+
+    // GET REQUEST KATEGOTIJE
+
+    public void prikaziKategorije() {
+
+        JsonArrayRequest request = new JsonArrayRequest(url, jsonArrayListener, errorListener);
+        requestQueue.add(request);
+
+    }
+
+    private Response.Listener<JSONArray> jsonArrayListener = new Response.Listener<JSONArray>() {
+        @Override
+        public void onResponse(JSONArray response) {
+            ArrayList<String> data = new ArrayList<>();
+
+
+            for (int i = 0; i < response.length(); i++) {
+                try {
+                    JSONObject object = response.getJSONObject(i);
+                    String id = object.getString("id");
+                    String name = object.getString("name");
+                    String category_image = object.getString("category_image");
+                    String description = object.getString("description");
+
+
+                    categID = id;
+                    //System.out.println(categID);
+                    data.add(id + " " + name + " " + "https://tilenkelc.eu/Eventer/public"  + category_image + " " + description);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    return;
+                }
+            }
+            System.out.println(Arrays.toString(data.toArray()));
+        }
+
+    };
+
+    private Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("REST error", error.getMessage());
+        }
+    };
+
+
+
+
+
+
+
+
+
+
     public String loadJSONFromAsset() {
         String json = null;
         try {
--- app/src/main/java/com/example/eventer/MasiRezervacija.java
+++ app/src/main/java/com/example/eventer/MasiRezervacija.java
@@ -1,6 +1,7 @@
 package com.example.eventer;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,24 +11,55 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.eventer.databinding.MasiRezervacijaBinding;
 
-public class MasiRezervacija extends Fragment{
-    private @NonNull MasiRezervacijaBinding binding;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MasiRezervacija extends Fragment {
+    private @NonNull
+    MasiRezervacijaBinding binding;
+    private String url = "https://tilenkelc.eu/Eventer/api/category/products/" + "1";
+    private RequestQueue requestQueue;
+    private String token;
+
+
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
 
+        token = "WFVPhlqSfjrJA3Sg8NvpL1brvQmTf8csPvCr6fCKaPzvg7vN74rwEOAnjq6a";
         binding = MasiRezervacijaBinding.inflate(inflater, container, false);
+        requestQueue = Volley.newRequestQueue(requireActivity().getApplicationContext());
+        prikaziProdukte();
+
         return binding.getRoot();
 
     }
+
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
+
         for (int table = 1; table < 13; table++) {
-            int idView = getResources().getIdentifier("Table" + table,"id",getContext().getPackageName());
+            int idView = getResources().getIdentifier("Table" + table, "id", getContext().getPackageName());
             View eventView = view.findViewById(idView);
             eventView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -36,7 +68,54 @@ public class MasiRezervacija extends Fragment{
                             .navigate(R.id.action_MasiRezervacija_to_FifthFragment);
                 }
             });
-            
+
         }
     }
+
+    public void prikaziProdukte() {
+
+        JsonArrayRequest req = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                ArrayList<String> data = new ArrayList<>();
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject object = response.getJSONObject(i);
+                        String id = object.getString("id");
+                        String name = object.getString("name");
+
+                        data.add(id + " " + name);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        return;
+                    }
+
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("REST error", error.getMessage());
+            }
+        })
+        {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Authorization", token);
+                headers.put("Accept", "application/json");
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+        };
+        requestQueue.add(req);
+    }
+
 }
+
+
+
